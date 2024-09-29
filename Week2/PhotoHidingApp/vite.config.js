4dd82359d480864fa40996a924ff10d42bf684e4
ankitsaw <ankitsaw950@gmail.com>
2024-09-30 04:43:46 +0530

    Add new file

--- Week2/PhotoHidingApp/vite.config.js
+++ Week2/PhotoHidingApp/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/photo-hiding-app/'  // Change this to your repository name
 })
